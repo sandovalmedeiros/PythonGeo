cf18833a876095fc4919914c01b7dddf767c2c2a
sandovalmedeiros <sandovalmedeiros@sei.ba.gov.br>
2023-02-09 13:34:18 -0300

    TesteRepositorio

--- testeRepositorio.py
+++ testeRepositorio.py
@@ -1,3 +1,17 @@
 #### Este é um teste do Repositório dos arquivos da capacitação
 #### Gerado em: 09/02/2023
+print()
+print('##### Exdrcício 01 Curso Python com Geo')
+print()
 print('Hello World!!!')
+print()
+### Coloquei mais umas linhas
+i=0
+for i in range(10):
+    if i == 0:
+        print('O i é igual a ' + str(i))
+        print()
+    else:
+        print('O i é diferente de zero é ' + str(i))
+        print()
+    i = i +1        
