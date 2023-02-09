ca8483b6754d1c1ff153972f6e00443a846528da
sandovalmedeiros <86790875+sandovalmedeiros@users.noreply.github.com>
2023-02-09 13:38:30 -0300

    Update testeRepositorio.py

--- testeRepositorio.py
+++ testeRepositorio.py
@@ -5,13 +5,18 @@ print('##### Exdrcício 01 Curso Python com Geo')
 print()
 print('Hello World!!!')
 print()
-### Coloquei mais umas linhas
+### Inicializa a variável i
 i=0
+# Faz a iteração dez vezes
 for i in range(10):
+    # Verifica se o i é zero
     if i == 0:
+        # Verdadeiro, informa que é zero
         print('O i é igual a ' + str(i))
         print()
     else:
+        # Senão, informa demais números
         print('O i é diferente de zero é ' + str(i))
         print()
+    # Incrementa a variávrl i    
     i = i +1        
